aac21b097f5ba1117508f6b8833d24c1b3eb25d7
Jack Keene <jack.keene@snowplowanalytics.com>
2023-05-10 18:53:30 +0100

    Add formData to channel group

--- src/schemata/channel_group.ts
+++ src/schemata/channel_group.ts
@@ -1,24 +1,32 @@
 export default {
     schema: {
-        "title": "Define channel group settings",
-        "type": "object",
-        "properties": {
-            "type": "object",
-            "channels": {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "properties": {
-                        "name": {
-                            "type": "string",
-                            "title": "Channel name"
+        title: "Define channel group settings",
+        type: "object",
+        properties: {
+            channels: {
+                type: "array",
+                items: {
+                    type: "object",
+                    properties: {
+                        name: {
+                            type: "string",
+                            title: "Channel name"
                         },
-                        "conditions": {
-                            "type": "object"
+                        condition: {
+                            type: "string",
+                            title: "Condition"
                         }
                     }
                 }
             }
         }
+    },
+    formData: {
+        channels:
+            [
+                {
+                    name: "Group A",
+                    condition: "app_id = 'conversion'",
+                }]
     }
 }
